91c64c1854534d8a321f720262570508071a660b
Karthikeyan Jayachandran <karthik.jm85@gmail.com>
2016-04-10 06:20:05 +0530

    Added date time custom preferences

---
+++ app/src/main/java/com/karthiknjay/mydays/dialog/DatePreference.java
@@ -0,0 +1,150 @@
+package com.karthiknjay.mydays.dialog;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.DatePicker;
+
+public class DatePreference extends DialogPreference {
+
+    private int lastDate = 0;
+    private int lastMonth = 0;
+    private int lastYear = 0;
+
+    private String dateval;
+
+    private CharSequence mSummary;
+
+    private DatePicker picker = null;
+
+    public static int getYear(String dateval) {
+        String[] pieces = dateval.split("-");
+        return (Integer.parseInt(pieces[0]));
+    }
+
+    public static int getMonth(String dateval) {
+        String[] pieces = dateval.split("-");
+        return (Integer.parseInt(pieces[1]));
+    }
+
+    public static int getDate(String dateval) {
+        String[] pieces = dateval.split("-");
+        return (Integer.parseInt(pieces[2]));
+    }
+
+    public DatePreference(Context context) {
+        this(context, null);
+    }
+
+    public DatePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPositiveButtonText("Set");
+        setNegativeButtonText("Cancel");
+    }
+
+    @Override
+    protected View onCreateDialogView() {
+        picker = new DatePicker(getContext());
+
+        // setCalendarViewShown(false) attribute is only available from API level 11
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            picker.setCalendarViewShown(false);
+        }
+
+        return (picker);
+    }
+
+    @Override
+    protected void onBindDialogView(View v) {
+        super.onBindDialogView(v);
+
+        picker.updateDate(lastYear, lastMonth, lastDate);
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+
+        if (positiveResult) {
+            lastYear = picker.getYear();
+            lastMonth = picker.getMonth();
+            lastDate = picker.getDayOfMonth();
+
+            String dateval = String.valueOf(lastYear) + "-"
+                    + String.format("%02d", lastMonth) + "-"
+                    + String.format("%02d", lastDate);
+
+            if (callChangeListener(dateval)) {
+                persistString(dateval);
+                updateSummary();
+            }
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return (a.getString(index));
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        dateval = null;
+
+        if (restoreValue) {
+            if (defaultValue == null) {
+                Calendar cal = Calendar.getInstance();
+                SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+                String formatted = format1.format(cal.getTime());
+                dateval = getPersistedString(formatted);
+            } else {
+                dateval = getPersistedString(defaultValue.toString());
+            }
+        } else {
+            dateval = defaultValue.toString();
+        }
+        lastYear = getYear(dateval);
+        lastMonth = getMonth(dateval);
+        lastDate = getDate(dateval);
+
+        updateSummary();
+    }
+
+    public void setText(String text) {
+        final boolean wasBlocking = shouldDisableDependents();
+
+        dateval = text;
+
+        persistString(text);
+
+        final boolean isBlocking = shouldDisableDependents();
+        if (isBlocking != wasBlocking) {
+            notifyDependencyChange(isBlocking);
+        }
+    }
+
+    public String getText() {
+        return dateval;
+    }
+
+    public void updateSummary() {
+        String day = String.valueOf(lastYear) + "-" + String.format("%02d", lastMonth+1)+ "-" + String.format("%02d", lastDate);
+        setSummary(day);
+    }
+
+    /*public CharSequence getSummary() {
+        return mSummary;
+    }*/
+
+   /* public void setSummary(CharSequence summary) {
+        if (summary == null && mSummary != null || summary != null
+                && !summary.equals(mSummary)) {
+            mSummary = summary;
+            notifyChanged();
+        }
+    }*/
+}
